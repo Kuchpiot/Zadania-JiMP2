6a02cc45e57fad10d83132e2e1c7791dd816dba2
Kuchpiot <kucharski@student.agh.edu.pl>
2017-03-24 18:01:08 +0100

    Not injection tests passed

--- lab4/simpletemplateengine/SimpleTemplateEngine.cpp
+++ lab4/simpletemplateengine/SimpleTemplateEngine.cpp
@@ -16,33 +16,66 @@ namespace nets
     std::string View::Render(const std::unordered_map <std::string, std::string> &model) const
     {
         // Rewrite pattern_ to be changed
-        std::string result = pattern_;
+        std::string inside_pattern = pattern_;
+
+        // Vector of changed indexes
+        std::vector <int> anti_injection;
+
+        std::string result = "";
+
+        bool can_rewrite = true;
+
+        std::cout << std::endl << inside_pattern << std::endl;
 
         // Change any word from model
         for(auto sample : model)
         {
             std::string replacement_to_find = "{{" + sample.first + "}}";
 
-            while (result.find(replacement_to_find) != std::string::npos)
+            while (inside_pattern.find( replacement_to_find ) != std::string::npos)
             {
-                result.replace(result.find(replacement_to_find), replacement_to_find.length(), sample.second);
+                // Check if
+                if(std::find( anti_injection.begin(), anti_injection.end(), inside_pattern.find( replacement_to_find ) ) == anti_injection.end())
+                {
+                    anti_injection.emplace_back( inside_pattern.find( replacement_to_find ) );
+                    inside_pattern.replace( inside_pattern.find( replacement_to_find ), replacement_to_find.length(), sample.second );
+                }
+                else
+                {
+
+                }
             }
         }
 
-        // Finding any non changed words
-        std::regex expression {R"((.+)?\{\{\w+\}\}(.+)?)"};
-        std::smatch matches;
+        int a = 0;
 
-        if(std::regex_match(result, matches, expression))
+        for(int i = 0; i < inside_pattern.length(); i++)
         {
-            std::cout << matches[1] << std::endl;
-            result.erase();
-
-            for(int i = 1; i < matches.size(); i++)
+            if(inside_pattern[i] == '{' && inside_pattern[i + 1] == '{' && inside_pattern[i + 2] != '{' &&
+                    std::find( anti_injection.begin(), anti_injection.end(), i ) == anti_injection.end())
             {
-                result.append(matches[i].str());
+                anti_injection.emplace_back(i);
+                a = i;
+                can_rewrite = false;
+            }
+            else if(inside_pattern[i - 1] == '}' && inside_pattern[i - 2] == '}' && inside_pattern[i - 3] != '}')
+            {
+                can_rewrite = true;
+            }
+
+            if(inside_pattern[i] == ' ' && !can_rewrite)
+            {
+                can_rewrite = true;
+                i = a;
+            }
+
+            if(can_rewrite)
+            {
+                result += inside_pattern[i];
             }
         }
+
+        std::cout << result << std::endl;
 
         return result;
     }
