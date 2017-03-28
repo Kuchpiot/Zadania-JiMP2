5006ea9a5979ce770dec1c179d2c2f9ed37838b5
Kuchpiot <kucharski@student.agh.edu.pl>
2017-03-28 09:55:16 +0200

    Json is not full, only constructors work but not at all

---
+++ lab4/netsjson/SimpleJson.cpp
@@ -0,0 +1,105 @@
+//
+// Created by piotr on 25.03.17.
+//
+
+
+#include <memory>
+#include "SimpleJson.h"
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+
+namespace nets
+{
+    JsonValue::JsonValue(int value)
+    {
+        my_value_ = new DataType(value);
+    }
+    JsonValue::JsonValue(double value)
+    {
+        my_value_ = new DataType(value);
+    }
+    JsonValue::JsonValue(bool value)
+    {
+        my_value_ = new DataType(value);
+    }
+    JsonValue::JsonValue(std::string value)
+    {
+        my_value_ = new DataType(value);
+    }
+    JsonValue::JsonValue(std::vector<JsonValue> value)
+    {
+        my_value_ = new DataType(value);
+    }
+    JsonValue::JsonValue(std::map<std::string, JsonValue> value)
+    {
+        my_value_ = new DataType(value);
+    }
+
+    std::experimental::optional<JsonValue> JsonValue::ValueByName(const std::string &name) const
+    {
+        if(my_value_->type == MAP_TYPE)
+        {
+            for (auto s : my_value_->map_value)
+            {
+                if (s.first == name)
+                {
+                    return std::experimental::optional<JsonValue>();
+                }
+            }
+        }
+    }
+
+    std::string JsonValue::ToString() const
+    {
+        switch (my_value_->type)
+        {
+            case INT_TYPE:
+                return std::to_string(my_value_->int_value);
+
+            case DOUBLE_TYPE:
+            {
+                std::stringstream stream;
+                stream << std::fixed << std::setprecision(2) << my_value_->double_value;
+                return stream.str();
+            }
+
+            case STRING_TYPE:
+                return "\"" + my_value_->string_value + "\"";
+
+            case BOOL_TYPE:
+                return my_value_->bool_value ? "true" : "false";
+
+            case VECTOR_TYPE:
+            {
+                std::string result = "[";
+
+                for (auto s : my_value_->vector_value)
+                {
+                    result += s.ToString().append(", ");
+                }
+
+                result = result.substr(0, result.length() - 2);
+
+                return result.append("]");
+            }
+
+            case MAP_TYPE:
+            {
+                std::string result2 = "";
+
+                std::cout << "Im on the case" << std::endl;
+
+                for (auto s : my_value_->map_value)
+                {
+                    std::cout << s.first << std::endl;
+                    result2.append("\"" + s.first + "\": " + s.second.ToString() + " ");
+                }
+
+                std::cout << "lel" << result2 << std::endl;
+
+                return result2;
+            }
+        }
+    }
+}
