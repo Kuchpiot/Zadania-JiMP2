1f34a67b5d8123a65c4ccb5dc9a9d1e226270dd9
kuchpiot <kucharski@student.agh.edu.pl>
2017-04-08 19:28:48 +0200

    My version of lab6

--- lab6/wordcounter/WordCounter.cpp
+++ lab6/wordcounter/WordCounter.cpp
@@ -2,4 +2,105 @@
 // Created by piotr on 06.04.17.
 //
 
+#include <algorithm>
 #include "WordCounter.h"
+
+namespace datastructures
+{
+    // WORD
+
+    std::string Word::GetWord()
+    {
+        return _word;
+    }
+
+    bool Word::operator ==(const Word &w2) const
+    {
+        return (this->_word == _word) ? true : false;
+    }
+
+
+    // COUNTS
+
+    int Counts::GetAmount()
+    {
+        return _amount;
+    }
+
+    void Counts::SetAmount(int a)
+    {
+        _amount = a;
+    }
+
+    void Counts::Increment()
+    {
+        _amount++;
+    }
+
+
+    // WORD COUNTER
+
+    WordCounter::WordCounter()
+    {
+        _wordCounterPairs = std::vector < std::pair <Word, Counts> > ();
+    }
+
+    WordCounter::WordCounter(std::string FileName)
+    {
+
+    }
+
+    WordCounter::WordCounter(std::initializer_list <Word> words)
+    {
+        _distinctWords = words.size();
+        for (auto &&item : words)
+        {
+//            if(std::find(_wordCounterPairs.begin(), _wordCounterPairs.end(), item) != _wordCounterPairs.end())
+//            {
+//
+//            }
+
+            bool has_word = false;
+
+            for (auto &&pair : _wordCounterPairs)
+            {
+                if(pair.first == item)
+                {
+                    has_word = true;
+                    pair.second.Increment();
+                    break;
+                }
+            }
+
+            if(!has_word)
+            {
+                std::pair <Word, Counts> newElement = std::pair <Word, Counts> (item, 1);
+                _wordCounterPairs.insert(_wordCounterPairs.end(), newElement);
+            }
+        }
+    }
+
+    unsigned long WordCounter::DistinctWords()
+    {
+        return _distinctWords;
+    }
+
+    unsigned long WordCounter::TotalWords()
+    {
+        return _wordCounterPairs.size();
+    }
+
+    std::set <Word> WordCounter::Words()
+    {
+        std::set <Word> words_stored;
+
+        for (auto &&item :_wordCounterPairs)
+        {
+            words_stored.insert(words_stored.end(), item.first);
+        }
+
+        return words_stored;
+    }
+
+
+}
--- lab6/wordcounter/WordCounter.h
+++ lab6/wordcounter/WordCounter.h
@@ -5,12 +5,56 @@
 #ifndef JIMP_EXERCISES_WORDCOUNTER_H
 #define JIMP_EXERCISES_WORDCOUNTER_H
 
+#include <string>
+#include <utility>
+#include <set>
+#include <vector>
+
+namespace datastructures
+{
+
+    class Word
+    {
+    public:
+        Word(std::string word) : _word(word) {}
+        std::string GetWord();
+        bool operator ==(const Word &w2) const;
+
+    private:
+        const std::string _word;
+    };
 
 
-class WordCounter {
+    class Counts
+    {
+    public:
+        Counts(int a) : _amount(a) {}
+        int GetAmount();
+        void SetAmount(int a);
+        void Increment();
 
-};
+    private:
+        int _amount;
+
+    };
 
 
+    class WordCounter
+    {
+    public:
+        WordCounter();
+        WordCounter(std::string FileName);
+        WordCounter(std::initializer_list <Word> words);
+
+        unsigned long DistinctWords();
+        unsigned long TotalWords();
+        std::set <Word> Words();
+
+    private:
+        std::vector < std::pair <Word, Counts> > _wordCounterPairs;
+        unsigned long _distinctWords;
+    };
+
+}
 
 #endif //JIMP_EXERCISES_WORDCOUNTER_H
