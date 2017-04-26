d84e2e056bc69556f421e955f7341f6e1ed84ded
kuchpiot <kucharski@student.agh.edu.pl>
2017-04-27 01:22:32 +0200

    Adds ex. 6

--- lab7/point/main.cpp
+++ lab7/point/main.cpp
@@ -7,10 +7,11 @@
 
 int main()
 {
+    Point *x = new Point();
     Point3D *a = new Point3D();
     Point3D *b = new Point3D(1, 2, 3);
 
-    std::cout << b->distance(*a) << std::endl;
+    std::cout << x->Distance(*a) << std::endl;
 
     delete a;
     delete b;
@@ -18,6 +19,12 @@ int main()
     return 0;
 }
 
+// Zadanie 5:
 // Najpierw wykonują się konstruktory klasy nadrzędnej, ponieważ jest ona wymagana do stworzenia
 // klasy potomej, której konstruktor wykonuje się w dalszej kolejności. Destruktory są wywoływane
 // w kolejności odwrotnej - najpierw wykonuje się destruktor klasy potomnej a potem klasy nadrzędnej
+
+// Zadanie 6:
+// Mieliśmy tutaj do czynienia ze zjawiskiem polimorfizmu - w związku z tym, że klasa Point3D dziedziczy
+// po klasie Point, jest ona też obiektem klasy Point, co pozwoliło na wykonanie metody Point::Distance(...)
+// gdzie Point3D został potraktowany jako obiekt Point (policzono ze współrzędnych x y)
