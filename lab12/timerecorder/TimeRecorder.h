3426648d5c56cc5ba6a8870305f37600043fc5e6
kuchpiot <kucharski@student.agh.edu.pl>
2017-06-11 01:14:58 +0200

    Drzewa się nie udały

--- lab12/timerecorder/TimeRecorder.h
+++ lab12/timerecorder/TimeRecorder.h
@@ -1,15 +1,31 @@
 //
-// Created by piotr on 05.06.17.
+// Created by zabdomi on 05.06.17.
 //
 
 #ifndef JIMP_EXERCISES_TIMERECORDER_H
 #define JIMP_EXERCISES_TIMERECORDER_H
 
 
+#include <utility>
+#include <chrono>
 
-class TimeRecorder {
+namespace profiling
+{
 
-};
+    template <class T>
+    std::pair<T, double> TimeRecorder(T* obj)
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+
+        auto v = obj();
+
+        auto end = std::chrono::high_resolution_clock::now() - start;
+
+        auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end);
+
+        return std::pair<T,double>(v, time.count());
+    };
+}
 
 
 
--- lab12/tree/TreeIterators.h
+++ lab12/tree/TreeIterators.h
@@ -5,13 +5,113 @@
 #ifndef JIMP_EXERCISES_TREEITERATORS_H
 #define JIMP_EXERCISES_TREEITERATORS_H
 
+#include <bits/unique_ptr.h>
+#include <vector>
+
 namespace tree
 {
     template <class T>
     class Tree
     {
+    public:
+        void Insert(T newVal)
+        {
+            if(!value_)
+            {
+                value_ = newVal;
+            }
+            else if(newVal <= value_)
+            {
+                if(!left_)
+                {
+                    left_ = std::unique_ptr<Tree> (newVal);
+                }
+                else
+                {
+                    left_->Insert(newVal);
+                }
+            }
+            else
+            {
+                if(!right_)
+                {
+                    right_ = std::unique_ptr<Tree> (newVal);
+                }
+                else
+                {
+                    right_->Insert(newVal);
+                }
+            }
+        }
 
+        Tree() {}
+        Tree(int v) : value_{v} {}
+
+        inline T Root() const { return value_; }
+
+        inline std::unique_ptr<Tree> GetLeft() { return left_; }
+        inline std::unique_ptr<Tree> GetRight() { return right_; }
+
+        operator int() { return int(value_); }
+
+    private:
+        T value_ = nullptr;
+        std::unique_ptr<Tree> left_ = nullptr;
+        std::unique_ptr<Tree> right_ = nullptr;
     };
+
+    template <class T>
+    std::vector<T> InOrder(Tree<T> tr)
+    {
+        std::vector<T> vals;
+        vals.push_back(InOrder(*tr.GetLeft()));
+        vals.push_back(tr.Root());
+        vals.push_back(InOrder(*tr.GetRight()));
+        return vals;
+    }
+
+    template <class T>
+    class InOrderTreeIterator
+    {
+    public:
+        InOrderTreeIterator(Tree<T> r) : node_{r} {}
+
+        T operator *() const { return node_.Root(); }
+        void operator ++() { node_ = *(node_.GetLeft()); }
+
+        Tree<T> getNode() { return node_; } const
+
+        bool operator !=(InOrderTreeIterator<T>& sec) const
+        {
+            return node_.Root() != sec.getNode().Root();
+        }
+
+    private:
+        Tree<T> node_;
+    };
+
+    template <class T>
+    class InOrderTreeView
+    {
+    public:
+        InOrderTreeView(Tree<T> tr)
+        {
+            InOrder(tr);
+        }
+
+        inline InOrderTreeIterator<T> begin() { return values_[0]; }
+        inline InOrderTreeIterator<T> end() { return values_[values_.size() - 1]; }
+
+        inline void AddToValues(T v)
+        {
+            values_.push_back(v);
+        }
+
+    private:
+        std::vector<T> values_;
+    };
+
+
 }
 
 #endif //JIMP_EXERCISES_TREEITERATORS_H
